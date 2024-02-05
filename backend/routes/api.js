b91cb6edd1123b9bc1f2387183ff733af594feae
Johan <johantoerme@live.se>
2024-02-05 16:04:13 +0100

    Excluding password from get /users with .project({password: 0})

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,8 +4,13 @@ var router = express.Router();
 //GET all users
 
 router.get('/users', function(req, res, next) {
-  req.app.locals.db.collection("users").find().toArray()
-  .then(results => {console.log(results);})
+  req.app.locals.db.collection("users").find().project({password: 0}).toArray()
+  .then(results => {
+
+    console.log(results);
+
+
+  })
   res.send('get users');
 });
 
