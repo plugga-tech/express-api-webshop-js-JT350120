22d12a0a6fe33b4e93c941169c94f36439404ece
Johan <johantoerme@live.se>
2024-02-05 21:47:47 +0100

    find user by ID works (when hardcoding ID)

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,21 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-//GET all users
+const ObjectId = require('mongodb').ObjectId;
 
+//Get all users
 router.get('/users', function(req, res, next) {
   req.app.locals.db.collection("users").find().project({password: 0}).toArray()
   .then(results => {
 
     console.log(results);
 
-
   })
   res.send('get users');
 });
 
-//POST new user
+//Get a specific user via id
+router.post('/users', function(req, res, next) {
+  req.app.locals.db.collection("users").find({_id: new ObjectId("65c0e96bc4957699d5ac238b")}).toArray()
+  .then(results => {
 
+    console.log(results);
+
+  })
+  res.send('get specific user');
+});
+
+//Create new user
 router.post('/users/add', function(req, res){
   req.app.locals.db.collection("users").insertOne(req.body)
   .then(result => {
