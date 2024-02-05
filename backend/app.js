86e609ead13ba08152a9e4f9d29a069c657e1cc8
Johan <johantoerme@live.se>
2024-02-05 15:34:39 +0100

    Post and get users works

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
-var app = express();
+const app = express();
 
 const MongoClient = require("mongodb").MongoClient;
 
@@ -25,7 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
---
+++ backend/routes/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+//GET all users
+
+router.get('/users', function(req, res, next) {
+  req.app.locals.db.collection("users").find().toArray()
+  .then(results => {console.log(results);})
+  res.send('get users');
+});
+
+//POST new user
+
+router.post('/users/add', function(req, res){
+  req.app.locals.db.collection("users").insertOne(req.body)
+  .then(result => {
+    res.send(result);
+  })
+})
+
+module.exports = router;
