b5133f62851f365a4161c7615c37721e62037554
Johan <johantoerme@live.se>
2024-02-06 14:13:18 +0100

    Able to get specific product via req.params.id

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const ObjectId = require('mongodb').ObjectId;
 
 //Get all users
+
 router.get('/users', function(req, res, next) {
   req.app.locals.db.collection("users").find().project({password: 0}).toArray()
   .then(results => {
@@ -15,6 +16,7 @@ router.get('/users', function(req, res, next) {
 });
 
 //Get a specific user via id
+
 router.post('/users', function(req, res, next) {
   req.app.locals.db.collection("users").find({_id: new ObjectId(req.body)}).toArray()
   .then(result => {
@@ -25,7 +27,8 @@ router.post('/users', function(req, res, next) {
   res.send('get specific user');
 });
 
-//Create new user
+//Create a new user
+
 router.post('/users/add', function(req, res){
   req.app.locals.db.collection("users").insertOne(req.body)
   .then(result => {
@@ -34,6 +37,7 @@ router.post('/users/add', function(req, res){
 })
 
 //Login
+
 router.post('/users/login', function(req, res, next) {
   req.app.locals.db.collection("users").find(req.body).toArray()
   .then(result => {
@@ -45,6 +49,32 @@ router.post('/users/login', function(req, res, next) {
     }
 
   })
+  res.send('get specific user');
+});
+
+//Create a new product
+
+router.post('/products/add', function(req, res){
+  req.app.locals.db.collection("products").insertOne(req.body)
+  .then(result => {
+    res.send(result);
+  })
+})
+
+//Get a specific product via ID in URL
+
+router.get('/products/:id', function(req, res, next) {
+  req.app.locals.db.collection("products").find({_id: new ObjectId(req.params.id)}).toArray()
+  .then(result => {
+
+    if (result == "") {
+      console.log("Produkten med ID " + req.params.id + " finns inte!");
+    } else {
+      console.log(result);
+    }
+
+  })
+  
   res.send('get specific user');
 });
 
